afe6e335ea8d87ebe1a8b3e20e4f691d135d57fa
michael <mw976@yale.edu>
2025-02-07 13:36:23 -0500

    Calculates number of regions for varying RSS coverage

--- src/contiguity.cpp
+++ src/contiguity.cpp
@@ -11,10 +11,12 @@
 
 using namespace std;
 
-map<unsigned long, int> region_sizes;
-vector<unsigned long> region_starts_V;
-vector<unsigned long> region_lengths;
-vector<unsigned long> region_starts_P;
+#define u64 unsigned long long
+
+map<u64, int> region_sizes;
+vector<u64> region_starts_V;
+vector<u64> region_lengths;
+vector<u64> region_starts_P;
 
 bool isInteger(const string& s) {
     // Check if s is a hexadecimal number (will not start with 0x)
@@ -26,12 +28,12 @@ bool isInteger(const string& s) {
 
 int main(int argc, char* argv[]) {
     string line;
-    unsigned long regions = 0;
-    unsigned long last_VPN = 0;
-    unsigned long last_PFN = 0;
-    unsigned long region_size = 0;
-    unsigned long total_pages = 0;
-    unsigned long lowest_VPN = UINT64_MAX;
+    u64 regions = 0;
+    u64 last_VPN = 0;
+    u64 last_PFN = 0;
+    u64 region_size = 0;
+    u64 total_pages = 0;
+    u64 lowest_VPN = UINT64_MAX;
     while (getline(cin, line)) {
         istringstream iss(line);
         string first, second;
@@ -40,8 +42,8 @@ int main(int argc, char* argv[]) {
         if (iss >> first >> second) {
             // Check if both are integers
             if (isInteger(first) && isInteger(second)) {
-                unsigned long VPN = stol(first, 0, 16) >> 12;
-                unsigned long PFN = stol(second, 0, 16) >> 12;
+                u64 VPN = stol(first, 0, 16) >> 12;
+                u64 PFN = stol(second, 0, 16) >> 12;
                 lowest_VPN = VPN < lowest_VPN ? VPN : lowest_VPN;
 
                 // Check if mapping is valid
@@ -98,35 +100,84 @@ int main(int argc, char* argv[]) {
         }
     }
     // Calculate the percentage of memory covered by the largest 32 and 128 regions
-    unsigned long largest_32 = 0;
-    unsigned long largest_128 = 0;
-    int i = 0;
+    // u64 largest_32 = 0;
+    // u64 largest_128 = 0;
+    // int i = 0;
+    // for (auto it = region_sizes.rbegin(); it != region_sizes.rend(); it++) {
+    //     u64 r_size = it->first;
+    //     int r_count = it->second;
+    //     if (i < 32) {
+    //         if (32 - i < r_count) {
+    //             largest_32 += (32 - i) * r_size;
+    //         }
+    //         else {
+    //             largest_32 += r_count * r_size;
+    //         }
+    //     }
+    //     if (i < 128) {
+    //         if (128 - i < r_count) {
+    //             largest_128 += (128 - i) * r_size;
+    //         }
+    //         else {
+    //             largest_128 += r_count * r_size;
+    //         }
+    //     }
+    //     i += r_count;
+    // }
+    // double p32 = (double)largest_32 / total_pages * 100;
+    // double p128 = (double)largest_128 / total_pages * 100;
+
+    // Get the number of regions needed to cover 75% of RSS (starting with the largest regions)
+    u64 r75 = 0, r50 = 0, r25 = 0;
+    u64 sz75 = (total_pages >> 2) * 3;
+    u64 sz50 = total_pages >> 1;
+    u64 sz25 = total_pages >> 2;
+    u64 cov = 0;
     for (auto it = region_sizes.rbegin(); it != region_sizes.rend(); it++) {
-        unsigned long r_size = it->first;
+        u64 r_size = it->first;
         int r_count = it->second;
-        if (i < 32) {
-            if (32 - i < r_count) {
-                largest_32 += (32 - i) * r_size;
+        u64 r_total = r_size * r_count;
+
+        // Check if coverage will exceed each size
+        if (cov < sz25) {
+            if (cov + r_total >= sz25) {
+                u64 diff = sz25 - cov;
+                r25 += diff / r_size;
+                r25 = diff % r_size == 0 ? r25 : r25 + 1;
             }
-            else {
-                largest_32 += r_count * r_size;
-            }
+            else r25 += r_count;
         }
-        if (i < 128) {
-            if (128 - i < r_count) {
-                largest_128 += (128 - i) * r_size;
+
+        if (cov < sz50) {
+            if (cov + r_total >= sz50) {
+                u64 diff = sz50 - cov;
+                r50 += diff / r_size;
+                r50 = diff % r_size == 0 ? r50 : r50 + 1;
             }
-            else {
-                largest_128 += r_count * r_size;
-            }
+            else r50 += r_count;
         }
-        i += r_count;
+
+        if (cov < sz75) {
+            if (cov + r_total >= sz75) {
+                u64 diff = sz75 - cov;
+                r75 += diff / r_size;
+                r75 = diff % r_size == 0 ? r75 : r75 + 1;
+            }
+            else r75 += r_count;
+        }
+        else break;
+
+        // Error cases
+        if (r75 < r50 || r75 < r25 || r50 < r25) {
+            cerr << "Error: " << r75 << " !< " << r50 << " !< " << r25 << endl;
+            return -1;
+        }
+        cov += r_total;
     }
-    double p32 = (double)largest_32 / total_pages * 100;
-    double p128 = (double)largest_128 / total_pages * 100;
 
     // Print the results
-    cout << dec << fixed << setprecision(5) << regions << "\t" << p32 << "%\t" << p128 << "%\t" << total_pages << "\t" << lowest_VPN << endl;
+    cout << dec << fixed << setprecision(5) << regions << "\t" << r75 << "\t" << r50 << "\t" << r25 << "\t";
+    cout << total_pages << "\t" << hex << lowest_VPN << endl;
 
     // Take an optional dump file as command line argument
     if (argc > 1) {
