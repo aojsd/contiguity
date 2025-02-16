ed2d275a9e6522d96c6ad465d83288c9f5827d52
michael <mw976@yale.edu>
2025-02-16 12:05:16 -0500

    Now runs all configurations for a given application

--- run_all.sh
+++ run_all.sh
@@ -1,24 +1,29 @@
-# Run trials of all applications using remote "run_pin.sh" script
-# Usage: ./run_all.sh <num_trials> <output_dir> <pin_mode> <THP>
+# Run trials of all configurations of an application using remote "run_pin.sh" script
+# Usage: ./run_all.sh <num_trials> <remote host> <output_dir> <THP>
 if [ "$#" -lt 4 ]; then
-    echo "Usage: ./pin_trials.sh <num_trials> <output_dir> <pin_mode> <THP: 0 or 1>"
+    echo "Usage: ./pin_trials.sh <num_trials> <remote host> <app> <output_dir> <Other>"
     exit 1
 fi
 
 # Setup script to stop all background jobs on interrupt
 trap 'kill $(jobs -p)' SIGINT
 
-# MemA
-mkdir -p $2/memA
-./contiguity_trials.sh $1 bingo memA $2/memA $3 $4 &
+# Rename args
+TRIALS=$1
+HOST=$2
+APP=$3
+OUTDIR=$4
+EXTRA=$5
+set -x
 
-# PR
-mkdir -p $2/pr
-./contiguity_trials.sh $1 gussie pr $2/pr $3 $4 &
+# Empty
+./contiguity_trials.sh $TRIALS $HOST $APP $OUTDIR/$APP empty $EXTRA
 
-# Llama
-mkdir -p $2/llama
-./contiguity_trials.sh $1 bertie llama $2/llama $3 $4 &
+# Disk
+./contiguity_trials.sh $TRIALS $HOST $APP $OUTDIR/$APP disk $EXTRA
 
-# Wait for all background jobs to finish
-wait $(jobs -p)
+# Disk skip
+./contiguity_trials.sh $TRIALS $HOST $APP $OUTDIR/$APP disk-skip $EXTRA
+
+# Fields
+./contiguity_trials.sh $TRIALS $HOST $APP $OUTDIR/$APP fields $EXTRA
