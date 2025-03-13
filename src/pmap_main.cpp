ee696ff80a30fcd439c22d38bdf56fc2ad6e3622
michael <mw976@yale.edu>
2025-03-13 17:41:40 -0400

    removed unnecessary comments

--- src/pmap_main.cpp
+++ src/pmap_main.cpp
@@ -78,18 +78,9 @@ int main(int argc, char **argv)
         // Check if mapping is valid
         if (PFN == 0) {
             if (region_size > 0) {
-                // if (region_sizes.find(region_size) == region_sizes.end()) {
-                //     region_sizes[region_size] = 1;
-                // }
-                // else {
-                //     region_sizes[region_size]++;
-                // }
                 n_regions++;
 
                 // Record region
-                // region_starts_V.push_back((last_VPN - region_size + 1) << 12);
-                // region_lengths.push_back(region_size);
-                // region_starts_P.push_back(last_PFN - region_size + 1);
                 total_pages += region_size;
 
                 // Get number of each power of 2 region
@@ -111,30 +102,15 @@ int main(int argc, char **argv)
             // New region started
             else {
                 if (region_size > 0) {
-                    // if (region_sizes.find(region_size) == region_sizes.end()) {
-                    //     region_sizes[region_size] = 1;
-                    // }
-                    // else {
-                    //     region_sizes[region_size]++;
-                    // }
                     n_regions++;
 
                     // Record region
-                    // region_starts_V.push_back((last_VPN - region_size + 1) << 12);
-                    // region_lengths.push_back(region_size);
-                    // region_starts_P.push_back(last_PFN - region_size + 1);
                     total_pages += region_size;
 
                     // Get number of each power of 2 region
                     u64 start = last_PFN - region_size + 1;
                     u64 end = last_PFN + 1;
                     count_pow2(start, end, CONT_HIGHEST, power2_regions);
-                    // region_size >>= 4;
-                    // for (int i = 4; i < 19; i++) {
-                    //     if (region_size == 0) break;
-                    //     power2_regions[i - 4] += region_size;
-                    //     region_size >>= 1;
-                    // }
                 }
                 region_size = 1;
             }
@@ -142,65 +118,6 @@ int main(int argc, char **argv)
             last_PFN = PFN;
         }
     }
-
-    //==================================================================================================
-    // Get the number of regions needed to cover X% of RSS (starting with the largest regions)
-    //==================================================================================================
-    // u64 r75 = 0, r50 = 0, r25 = 0;
-    // u64 sz75 = (total_pages >> 2) * 3;
-    // u64 sz50 = total_pages >> 1;
-    // u64 sz25 = total_pages >> 2;
-    // u64 cov = 0;
-    // for (auto it = region_sizes.rbegin(); it != region_sizes.rend(); it++) {
-    //     u64 r_size = it->first;
-    //     int r_count = it->second;
-    //     u64 r_total = r_size * r_count;
-
-    //     // Check if coverage will exceed each size
-    //     if (cov < sz25) {
-    //         if (cov + r_total >= sz25) {
-    //             u64 diff = sz25 - cov;
-    //             r25 += diff / r_size;
-    //             r25 = diff % r_size == 0 ? r25 : r25 + 1;
-    //         }
-    //         else r25 += r_count;
-    //     }
-
-    //     if (cov < sz50) {
-    //         if (cov + r_total >= sz50) {
-    //             u64 diff = sz50 - cov;
-    //             r50 += diff / r_size;
-    //             r50 = diff % r_size == 0 ? r50 : r50 + 1;
-    //         }
-    //         else r50 += r_count;
-    //     }
-
-    //     if (cov < sz75) {
-    //         if (cov + r_total >= sz75) {
-    //             u64 diff = sz75 - cov;
-    //             r75 += diff / r_size;
-    //             r75 = diff % r_size == 0 ? r75 : r75 + 1;
-    //         }
-    //         else r75 += r_count;
-    //     }
-    //     else break;
-
-    //     // Error cases
-    //     if (r75 < r50 || r75 < r25 || r50 < r25) {
-    //         cerr << "Error: " << r75 << " !< " << r50 << " !< " << r25 << endl;
-    //         return -1;
-    //     }
-    //     cov += r_total;
-    // }
-
-    //==================================================================================================
-    // Get the base addresses of tracked virtual mappings in sorted order
-    //==================================================================================================
-    // vector<u64> base_addrs;
-    // for (auto r : largestRegions) {
-    //     base_addrs.push_back(r.address);
-    // }
-    // sort(base_addrs.begin(), base_addrs.end());
 
     //==================================================================================================
     // Print results
@@ -216,11 +133,5 @@ int main(int argc, char **argv)
     for (u64 r : power2_regions) {
         cout << "," << r;
     }
-    // cout << dec << fixed << setprecision(3) << n_regions << "\t" << r75 << "\t" << r50 << "\t" << r25 << "\t";
-    // cout << tracked_rss_gb << "GB\t" << rss_gb << "GB\t" << largestRegions.size() << "\t";
-    // cout << hex << base_addrs[0];
-    // for (size_t i = 1; i < base_addrs.size(); i++) {
-    //     cout << "___" << base_addrs[i];
-    // }
     cout << endl;
 }
