3f9aedb611a6a22ad641d0a21ac5c3d81c701ccb
michael <mw976@yale.edu>
2025-08-03 02:20:01 -0400

    added pause instruction tracking

--- live_profilers.sh
+++ live_profilers.sh
@@ -24,12 +24,12 @@ sudo ${CONT_DIR}/kernel_work/kthread_cputime.bt > ${TMP_DIR}/${process_name}.kth
 sudo ${CONT_DIR}/kernel_work/pid_syscall_profiler.bt ${pid} > ${TMP_DIR}/${process_name}.syscalls &
 
 # Attach perf to the whole process
-sudo perf stat -e instructions,L1-dcache-loads,L1-dcache-stores -p ${pid} -a &> ${TMP_DIR}/${process_name}.perf &
+EVENTS="instructions,L1-dcache-loads,L1-dcache-stores,rob_misc_events.pause_inst"
+sudo perf stat -e ${EVENTS} -p ${pid} -a &> ${TMP_DIR}/${process_name}.perf &
 
 # Profile threads of the process
 #  - If name contains 'pr', wait for 20s so all threads can start
 if [[ "$process_name" == *"pr"* ]]; then
     sleep 20
 fi
-EVENTS="instructions,L1-dcache-loads,L1-dcache-stores"
 sudo perf stat --per-thread -e ${EVENTS} -p ${pid} -a &> ${TMP_DIR}/${process_name}_threads.perf &
